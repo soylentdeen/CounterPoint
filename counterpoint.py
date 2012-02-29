aeb74f349718aa89edc3755d9b5649f4929e8eb0
Casey Deen <deen@mpia.de>
2012-02-29 16:46:21 +0100

    fixed formatting

--- counterpoint.py
+++ counterpoint.py
@@ -4,7 +4,8 @@ import os
 import MOOGTools
 
 def write_par_file(wl_start, wl_stop, datadir, prefix, index, zeeman_prefix):
-    outfile_name = os.path.join(datadir, zeeman_prefix)+'/'+prefix+str(index)+'.par'
+    outfile_name = os.path.join(datadir, zeeman_prefix)+'/'+
+                   prefix+str(index)+'.par'
     pf = open(outfile_name, 'w')
 
     stronglines = True
@@ -15,7 +16,8 @@ def write_par_file(wl_start, wl_stop, datadir, prefix, index, zeeman_prefix):
     pf.write('standard_out  \'./output/out1\'\n')
     pf.write('lines_in      \'./'+prefix+'weak_linelist_'+str(index)+'.out\'\n')
     if stronglines:
-        pf.write('stronglines_in      \'./'+prefix+'strong_linelist_'+str(index)+'.out\'\n')
+        pf.write('stronglines_in      \'./'+prefix+
+                 'strong_linelist_'+str(index)+'.out\'\n')
         pf.write('strong          1\n')
     pf.write('atmosphere      1\n')
     pf.write('molecules       2\n')
@@ -43,9 +45,11 @@ def write_par_file(wl_start, wl_stop, datadir, prefix, index, zeeman_prefix):
         #    gravs = range(300, 550, 50)
         for G in gravs:
             pf.write('RUN            '+str(run_number)+'\n')
-            pf.write('smoothed_out   \'./output/'+prefix+'MARCS_T'+str(T)+'G'+str(G)+'_'+str(index)+'.moog\'\n')
+            pf.write('smoothed_out   \'./output/'+prefix+
+                    'MARCS_T'+str(T)+'G'+str(G)+'_'+str(index)+'.moog\'\n')
             pf.write('hardpost_out   \'./output/dummy.ps\'\n')
-            pf.write('model_in       \'../../../../atmospheres/MARCS/MARCS_T'+str(T)+'_G'+str(G/100.0)+'_M0.0_t1.0.md\'\n')
+            pf.write('model_in       \'../../../../atmospheres/MARCS/MARCS_T'+
+                    str(T)+'_G'+str(G/100.0)+'_M0.0_t1.0.md\'\n')
             pf.write('abundances     1  1\n')
             pf.write('    12      0.0\n')
             run_number += 1
@@ -83,8 +87,8 @@ def generateLineList(datadir, prefix, wl_start, wl_stop, Bfield):
             glow = float(l[6])
             gup = float(l[7])
             VdW = float(l[9])
-            weakLines.append(MOOGTools.spectral_Line(wave, species, exp_pot, loggf, Jlow=Jlow, Jup=Jup, glow=glow, gup=gup,
-            VdW=VdW))
+            weakLines.append(MOOGTools.spectral_Line(wave, species, exp_pot,
+                loggf, Jlow=Jlow, Jup=Jup, glow=glow, gup=gup, VdW=VdW))
             weakLines[-1].zeeman_splitting(Bfield)
 
     for line in open(molecules, 'r').readlines():
@@ -95,7 +99,8 @@ def generateLineList(datadir, prefix, wl_start, wl_stop, Bfield):
             exp_pot = float(l[2])
             loggf = float(l[3])
             DE = float(l[4])
-            weakLines.append(MOOGTools.spectral_Line(wave, species, exp_pot, loggf, DissE=DE))
+            weakLines.append(MOOGTools.spectral_Line(wave, species, exp_pot,
+                loggf, DissE=DE))
 
     weakLines.sort()
 
@@ -113,8 +118,8 @@ def generateLineList(datadir, prefix, wl_start, wl_stop, Bfield):
             glow = float(l[6])
             gup = float(l[7])
             VdW = float(l[9])
-            strongLines.append(MOOGTools.spectral_Line(wave, species, exp_pot, loggf, Jlow=Jlow, Jup=Jup, glow=glow,
-            gup=gup, VdW=VdW))
+            strongLines.append(MOOGTools.spectral_Line(wave, species, exp_pot,
+                loggf, Jlow=Jlow, Jup=Jup, glow=glow, gup=gup, VdW=VdW))
             strongLines[-1].zeeman_splitting(Bfield)
 
     strongLines.sort()
@@ -138,32 +143,40 @@ def generateLineList(datadir, prefix, wl_start, wl_stop, Bfield):
         i = 0
         j = 0
         strong_file_counter = 0
-        sfn = os.path.join(datadir,zp)+'/'+prefix+'strong_linelist_'+str(strong_file_counter)+'.out'
+        sfn = os.path.join(datadir,zp)+'/'+prefix+'strong_linelist_'+
+              str(strong_file_counter)+'.out'
         outfile = open(sfn, 'w')
         strong_line_counter = 0
         breakpoints = []
         breakpoints.append(weakLines[0].wl)
 
         while i < len(strongLines):
-            if (strong_line_counter + len(strongLines[i].zeeman[zp][0])) < strong_line_limit:
+            if (strong_line_counter + 
+                    len(strongLines[i].zeeman[zp][0])) < strong_line_limit:
                 strongLines[i].dump(out=outfile, zeeman=zp, mode='MOOG')
                 strong_line_counter += len(strongLines[i].zeeman[zp][0])
                 i = i + 1
             else:
-                breakpoints.append(numpy.mean([strongLines[i].wl, strongLines[i-1].wl]))
+                breakpoints.append(numpy.mean([strongLines[i].wl,
+                    strongLines[i-1].wl]))
                 j = i
-                while ( (strong_line_counter < strong_line_limit) & (j < len(strongLines) ) ):
+                while ( (strong_line_counter < strong_line_limit) &
+                        (j < len(strongLines) ) ):
                     strongLines[j].dump(out=outfile, mode='MOOG')
                     strong_line_counter += 1
                     j += 1
                 outfile.close()
                 sort_file(sfn)
 
-                wfn = os.path.join(datadir,zp)+'/'+prefix+'weak_linelist_'+str(strong_file_counter)+'.out'
+                wfn = os.path.join(datadir,zp)+'/'+prefix+'weak_linelist_'+
+                          str(strong_file_counter)+'.out'
                 weakfile = open(wfn, 'w')
-                bm = scipy.where( (weakLines > breakpoints[-2]) & (weakLines < breakpoints[-1]) )[0]
+                bm = scipy.where( (weakLines > breakpoints[-2]) &
+                        (weakLines < breakpoints[-1]) )[0]
                 if len(bm) == 0:
-                    weakfile.write('%10.3f%10s%10.3f%10.3f\n' % (numpy.mean([breakpoints[-2], breakpoints[-1]]), 26.0, 6.0, -8.0))
+                    weakfile.write('%10.3f%10s%10.3f%10.3f\n' %
+                            (numpy.mean([breakpoints[-2], breakpoints[-1]]),
+                            26.0, 6.0, -8.0))
                 for k in bm:
                     if not(weakLines[k] in strongLines):
                         weakLines[k].dump(out=weakfile, zeeman=zp, mode='MOOG')
@@ -172,23 +185,25 @@ def generateLineList(datadir, prefix, wl_start, wl_stop, Bfield):
                 sort_file(wfn)
             
 
-                write_par_file(breakpoints[-2], breakpoints[-1], datadir, prefix, strong_file_counter, zp)
+                write_par_file(breakpoints[-2], breakpoints[-1], datadir,
+                        prefix, strong_file_counter, zp)
 
                 strong_file_counter += 1
-                sfn=os.path.join(datadir,zp)+'/'+prefix+'strong_linelist_'+str(strong_file_counter)+'.out'
+                sfn=os.path.join(datadir,zp)+'/'+prefix+'strong_linelist_'+
+                        str(strong_file_counter)+'.out'
                 outfile = open(sfn, 'w')
                 strongLines[i-1].dump(out=outfile, mode='MOOG')
                 strong_line_counter = 1
  
         outfile.close()
         breakpoints.append(weakLines[-1].wl)
-        write_par_file(breakpoints[-2], breakpoints[-1], datadir, prefix, strong_file_counter,zp)
+        write_par_file(breakpoints[-2], breakpoints[-1], datadir, prefix,
+                strong_file_counter,zp)
 
-        wfn = os.path.join(datadir,zp)+'/'+prefix+'weak_linelist_'+str(strong_file_counter)+'.out'
+        wfn = os.path.join(datadir,zp)+'/'+prefix+'weak_linelist_'+
+                str(strong_file_counter)+'.out'
         weakfile = open(wfn, 'w')
-        #bm = scipy.where( (weakLines > breakpoints[-2]) & (weakLines < breakpoints[-2]+(breakpoints[-1]-breakpoints[-2])) )[0]
 
-        #for k in bm:
         for k in range(len(weakLines)):
             if not(weakLines[k] in strongLines):
                 weakLines[k].dump(out=weakfile, zeeman=zp, mode='MOOG')
@@ -213,4 +228,5 @@ for B in [0.0, 2.0]:
         wl_start = feature[1]
         wl_stop = feature[2]
         datadir = 'test_B_'+str(B)+'kG'
-        generateLineList(datadir, prefix, wl_start*10000.0, wl_stop*10000.0, B/10.0)
+        generateLineList(datadir, prefix, wl_start*10000.0,
+                wl_stop*10000.0, B/10.0)

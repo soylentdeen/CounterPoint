a3b7ebaed7db3c11f9458bb3b2e219aca5647f3f
Casey Deen <deen@mpia.de>
2013-06-27 16:43:19 +0200

    Preparing for release version 1.0!

--- counterpoint.py
+++ counterpoint.py
@@ -5,66 +5,8 @@ import MoogTools
 import AstroUtils
 import sys
 
-def write_par_file(wl_start, wl_stop, stage_dir, b_dir, prefix,
-        temps=None, gravs=None, **kwargs):
-    outfile_name = os.path.join(stage_dir,'Parfiles',b_dir,'batch.par')
-    pf = open(outfile_name, 'w')
 
-    stronglines = True
-
-    pf.write('gridstokes\n')
-    pf.write('terminal      \'x11\'\n')
-    pf.write('summary_out   \'../../Output/'+b_dir+'/summary.out\'\n')
-    pf.write('smoothed_out  \'../../Output/'+b_dir+'/junk.out\'\n')
-    pf.write('standard_out  \'../../Output/'+b_dir+'/out1\'\n')
-    pf.write('atmos_dir     \'/home/deen/Data/Atmospheres/MARCS/\'\n')
-    pf.write('out_dir       \'../../Output/'+b_dir+'/\'\n')
-    pf.write('lines_in      \'../../Linelists/'+b_dir+'/'+prefix
-            +'_weak_linelist.out\'\n')
-    if stronglines:
-        pf.write('stronglines_in      \'../../Linelists/'+b_dir+'/'+prefix+
-                 '_strong_linelist.out\'\n')
-        pf.write('strong          1\n')
-    pf.write('atmosphere      1\n')
-    pf.write('molecules       2\n')
-    pf.write('lines           1\n')
-    pf.write('damping         0\n')
-    pf.write('freeform        0\n')
-    pf.write('ncells          695\n')
-    pf.write('nrings          23\n')
-    #if (theta != -99.0):
-    #    pf.write('flux/int        1\n')
-    #else:
-    #    pf.write('flux/int        0\n')
-    pf.write('plot            1\n')
-    pf.write('synlimits\n')
-    pf.write('                '+str(wl_start)+' '+str(wl_stop)+' 0.05 1.00\n')
-    pf.write('plotpars        1\n')
-    pf.write('                '+str(wl_start)+' '+str(wl_stop)+' 0.05 1.01\n')
-    pf.write('                0.0 0.000 0.000 1.00\n')
-    pf.write('                g 0.000 0.00 0.00 0.00 0.00\n')
-    
-    run_number = 1
-
-    if (not temps):
-        temps = range(2500, 4100, 100)+range(4250, 6250, 250)
-    if (not gravs):
-        gravs = range(300, 550, 50)
-    
-    for T in temps:
-        for G in gravs:
-            pf.write('RUN            '+str(run_number)+'\n')
-            pf.write('stokes_out   \''+prefix+
-                    '_MARCS_T'+str(T)+'G'+str(G)+'\'\n')
-            pf.write('hardpost_out   \'../../Output/'+b_dir+'/dummy.ps\'\n')
-            pf.write('model_in       \'MARCS_T'+
-                    str(T)+'_G'+str(G/100.0)+'_M0.0_t2.0.md\'\n')
-            pf.write('abundances     1  1\n')
-            pf.write('    12      0.0\n')
-            run_number += 1
-    pf.close()
-
-def sort_file(name):
+def sort_file(name, weak=False):
     data = open(name, 'r').readlines()
     wl = []
     for line in data:
@@ -72,6 +14,8 @@ def sort_file(name):
 
     order = numpy.argsort(wl)
     out = open(name, 'w')
+    if weak:
+        out.write("Weak Lines\n")
     for i in order:
         out.write(data[i])
     out.close()
@@ -94,37 +38,28 @@ def generateLineList(b_dir, prefix, wl_start, wl_stop, Bfield, **kwargs):
             mode='gridstokes', strongLines=True, **kwargs)
 
     # Load in configuration file
-    config = AstroUtils.parse_config('cp.cfg')
+    config = AstroUtils.parse_config('counterpoint.cfg')
     weak_file = config['weak_file']
     strong_file = config['strong_file']
     molecules = config['molecules']
     VALD_list = config['VALD_file']
 
     strongLines, weakLines = MoogTools.parse_VALD(VALD_list, strong_file,
-            molecules, wl_start, wl_stop, Bfield)
+            wl_start, wl_stop, Bfield)
 
-    '''
-    if ( len(weakLines) == 0 ):
-        wave = (wl_start+wl_stop)/2.0
-        species = 26.0
-        exp_pot = 9.99
-        loggf = -9.99
-        Jlow = 1.0
-        Jup = 1.0
-        glow = 0.1
-        gup = 0.1
-        VdW = 0.0
-        weakLines.append(MOOGTools.spectral_Line(wave, species, exp_pot,
-            loggf, Jlow=Jlow, Jup=Jup, glow=glow, gup=gup, VdW=VdW))
-        weakLines[-1].zeeman_splitting(Bfield)
-    '''
+    #     CO
+    #weakLines = numpy.append(weakLines, MoogTools.parse_HITRAN(
+    #         molecules+'05_HITEMP2010new.par', wl_start, wl_stop, Bfield))
+    #     OH
+    #weakLines = numpy.append(weakLines, MoogTools.parse_HITRAN(
+    #         molecules+'13_HITEMP2010.par', wl_start, wl_stop, Bfield))
 
     sfn = os.path.join(staging_dir, 'Linelists', b_dir,
             prefix+'_strong_linelist.stokes')
     outfile = open(sfn, 'w')
 
     for strongLine in strongLines:
-        strongLine.dump(out=outfile, mode='MOOG')
+        strongLine.dump(out=outfile, mode='MOOGSTOKES')
     outfile.close()
     sort_file(sfn)
 
@@ -132,10 +67,10 @@ def generateLineList(b_dir, prefix, wl_start, wl_stop, Bfield, **kwargs):
             prefix+'_weak_linelist.stokes')
     weakfile = open(wfn, 'w')
     for weakLine in weakLines:
-        weakLine.dump(out=weakfile, mode='MOOG')
+        weakLine.dump(out=weakfile, mode='MOOGSTOKES')
 
     weakfile.close()
-    sort_file(wfn)
+    sort_file(wfn, weak=True)
 
     if Bfield==0.0:
         MoogTools.write_par_file(wl_start, wl_stop, staging_dir, b_dir,
@@ -157,7 +92,7 @@ def generateLineList(b_dir, prefix, wl_start, wl_stop, Bfield, **kwargs):
         for weakLine in weakLines:
             weakLine.dump(out=outfile, mode='MOOGSCALAR')
         outfile.close()
-        sort_file(sfn)
+        sort_file(wfn, weak=True)
 
 
 parameters = AstroUtils.parse_config(sys.argv[-1])
